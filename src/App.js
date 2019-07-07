32585e2a5fa62a63f06f5259344fde235e789e19
Suha <abdullahsuhaisik@gmail.com>
2019-07-07 16:50:47 +0300

    init commit and landingHeader uı is done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LandingScreen from './features/LandingPage/LandingScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LandingScreen />
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/Buttons/Button.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  display: inline-block;
+  background: #e50914;
+  color: #fff;
+  padding: 0.4rem 1.3rem;
+  font-size: 1rem;
+  text-align: center;
+  cursor: pointer;
+  margin-right: 0.5rem;
+  outline: none;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
+  border-radius: 2px;
+  &:hover {
+    opacity: 0.9;
+  }
+  border-radius: ${props => props.borderRadios || '0px'};
+`;
+
+const Button = ({ children, onclick, styles,borderRadios }) => {
+  return (
+    <ButtonWrapper onClick={onclick} style={styles} borderRadios={borderRadios}>
+      {children}
+    </ButtonWrapper>
+  );
+};
+
+export default Button;
